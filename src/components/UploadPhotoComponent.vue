990448cdc9ee9f0ea8d9f4a5788ddf9bc9c21dea
Dvalenciao1 <dv237968@gmail.com>
2023-08-16 13:28:45 -0500

    init project

---
+++ src/components/UploadPhotoComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-card flat class="my-card">
+    <q-card-section>
+      <q-img
+      class="fit"
+
+        height="350px"
+        src="https://placeimg.com/40/10/nature"
+        spinner-color="primary"
+        spinner-size="82px"
+      >
+        <template v-slot:error>
+          <div class="absolute-full flex flex-center bg-negative text-white">
+            Cannot load image
+          </div>
+        </template>
+      </q-img>
+    </q-card-section>
+    <q-card-section>
+      <q-btn class="fit" color="positive" label="capturar o subir imagen" />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+    <q-header elevated style="height: 70px">
+      <q-toolbar class="q-pa-lg">
+        <q-toolbar-title> Traductor Braille</q-toolbar-title>
+        <q-btn dense flat rounded color="bg-white" icon="fa-regular fa-heart" />
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts"></script>
